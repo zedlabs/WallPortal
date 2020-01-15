51420a427e47b5bf071fe3162ccd674514e19bab
zohaib <zohaibansari100@gmail.com>
2020-01-15 12:21:54 +0530

    bottom navigation theme setup/fix.

--- app/src/main/java/tk/zedlabs/wallaperapp2019/repository/BookmarkDatabase.kt
+++ app/src/main/java/tk/zedlabs/wallaperapp2019/repository/BookmarkDatabase.kt
@@ -2,8 +2,7 @@ package tk.zedlabs.wallaperapp2019.repository
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import tk.zedlabs.wallaperapp2019.repository.BookmarkDao
-import tk.zedlabs.wallaperapp2019.repository.BookmarkImage
+
 
 @Database(entities = arrayOf(BookmarkImage::class),version = 1)
 abstract class BookmarkDatabase : RoomDatabase() {
