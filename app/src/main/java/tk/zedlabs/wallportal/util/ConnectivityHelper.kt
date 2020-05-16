a30be61db58ee12ec6b7004c9ce60fd0ef60b880
GeorgCantor <cupsman1986@gmail.com>
2020-05-16 18:23:41 +0300

    cleaned BookmarkAdapter, ConnectivityHelper code

--- app/src/main/java/tk/zedlabs/wallportal/util/ConnectivityHelper.kt
+++ app/src/main/java/tk/zedlabs/wallportal/util/ConnectivityHelper.kt
@@ -3,11 +3,10 @@ package tk.zedlabs.wallportal.util
 import android.content.Context
 import android.net.ConnectivityManager
 
-class ConnectivityHelper(context: Context) {
+class ConnectivityHelper(private val context: Context) {
 
-    val mContext = context
     fun isConnectedToNetwork(): Boolean {
-        val connectivityManager = mContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
         return connectivityManager?.activeNetworkInfo?.isConnectedOrConnecting ?: false
     }
 }
