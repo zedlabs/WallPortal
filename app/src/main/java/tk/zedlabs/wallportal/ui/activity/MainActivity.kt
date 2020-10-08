21c2b627f9350a44403aad3e0dcaa2f9f4c75054
zohaib <zohaibansari100@gmail.com>
2020-10-08 20:54:44 +0530

    fixes bottom navigation

--- app/src/main/java/tk/zedlabs/wallportal/ui/activity/MainActivity.kt
+++ app/src/main/java/tk/zedlabs/wallportal/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package tk.zedlabs.wallportal.ui.activity
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,6 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import tk.zedlabs.wallportal.R
+import tk.zedlabs.wallportal.util.makeFadeTransition
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,14 +25,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),1)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1
+            )
         }
 
         toolbar.title = getString(R.string.app_name)
         navController = Navigation.findNavController(this, R.id.fragment)
         bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailActivity, R.id.originalResolutionFragment -> {
+                    bottomNavigation.makeFadeTransition(300)
+                    bottomNavigation.visibility = View.GONE
+                }
+                else -> bottomNavigation.visibility = View.VISIBLE
+            }
+
+        }
 
         //todo add settings bottom navigation tab
     }
