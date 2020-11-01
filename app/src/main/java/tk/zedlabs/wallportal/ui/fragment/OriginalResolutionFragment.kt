d5532aa1d80f128449e1ce141be4937be4b382d8
zohaib <zohaibansari100@gmail.com>
2020-11-01 20:18:48 +0530

    adds viewBinding in OriginalResolutionFragment.kt

--- app/src/main/java/tk/zedlabs/wallportal/ui/fragment/OriginalResolutionFragment.kt
+++ app/src/main/java/tk/zedlabs/wallportal/ui/fragment/OriginalResolutionFragment.kt
@@ -2,7 +2,9 @@ package tk.zedlabs.wallportal.ui.fragment
 
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -12,15 +14,28 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_original_resolution.*
 import tk.zedlabs.wallportal.R
+import tk.zedlabs.wallportal.databinding.ActivityOriginalResolutionBinding
 import tk.zedlabs.wallportal.util.shortToast
 
 @AndroidEntryPoint
-class OriginalResolutionFragment : Fragment(R.layout.activity_original_resolution) {
+class OriginalResolutionFragment : Fragment() {
 
     private val navArgs: OriginalResolutionFragmentArgs by navArgs()
 
+    private var _binding: ActivityOriginalResolutionBinding? = null
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ActivityOriginalResolutionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         Glide
@@ -33,9 +48,9 @@ class OriginalResolutionFragment : Fragment(R.layout.activity_original_resolutio
                 override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap?>?,
                                              dataSource: com.bumptech.glide.load.DataSource?, isFirstResource: Boolean): Boolean {
                     if (resource != null) {
-                        textViewLoading.visibility = View.GONE
-                        progressBar.visibility = View.GONE
-                        or_res_cl.setBackgroundColor(
+                        binding.textViewLoading.visibility = View.GONE
+                        binding.progressBar.visibility = View.GONE
+                        binding.orResCl.setBackgroundColor(
                             Palette.from(resource).generate().getDarkVibrantColor(
                                 ContextCompat.getColor(context!!, R.color.grey)
                             )
@@ -46,10 +61,16 @@ class OriginalResolutionFragment : Fragment(R.layout.activity_original_resolutio
 
                 override fun onLoadFailed(e: GlideException?,model: Any?,target: Target<Bitmap?>?,isFirstResource: Boolean): Boolean {
                     requireContext().shortToast(e?.message ?: "")
-                    textViewLoading.text = getString(R.string.failed_to_load)
+                    binding.textViewLoading.text = getString(R.string.failed_to_load)
                     return false
                 }
             })
-            .into(image_view_original)
+            .into(binding.imageViewOriginal)
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
